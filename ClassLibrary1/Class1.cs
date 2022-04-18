c9f17ea7305c90aa0e311d4b9617f12049b820b2
Даниил <kourin.daniil@gmail.com>
2022-04-18 09:52:19 +0300

    Экзамен

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private static Class1 class1;
+
+        private static Class1 GetClass1()
+        {
+            return Class1;
+
+        }
+        private static void CreateTaskList(string name)
+        { 
+    private static string name;
+    private static List<Task> tasks;
+}
+    }
+}
